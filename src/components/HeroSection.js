a9597a59ce8e5d7a33e81ec02a9689e3b7c5e46f
ckp8 <charanjit127@hotmail.com>
2021-02-17 20:13:24 +0000

    commiting changes

---
+++ src/components/HeroSection.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Button} from './pages/Button'
+import {Link} from 'react-router-dom'
+import './hero.css'
+
+export default function Hero({
+    lightbg , topLine, lightText, lightTextDesc, headLine, description, buttonLabel, img, alt, imgStart
+}) {
+    return (
+        <>
+          <div className = {lightbg ? 'home__hero-section' : 'home__section darkBg'} >
+              <div className = 'container'>
+                  <div className = "row home__hero-row" 
+                  style = {{display: 'flex', flexDirection: imgStart === "start" ? 'row-reverse': 'row'}}
+                  >
+<div className = 'col'>
+    <div className = "home__hero-text-wrapper">
+       <div className = "top-line">
+          {topLine} </div> 
+    </div>
+    <h1 className={lightText ? 'heading' : 'heading dark' }>
+        {headLine}
+    </h1>
+    <p className = {lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description}
+    </p>
+    <Link to = '/sign up'>
+        <Button buttonSize = 'btn--wide' buttonColor = 'blue'>
+            {buttonLabel}
+        </Button>
+    </Link>
+</div>
+                  </div>
+                  <div className ='col'>
+                      <div className = "home__hero-img-wrapper">
+                          <img src ={img} alt = {alt} className ="home__hero-img"/>
+                      </div>
+                  </div>
+              </div>
+              </div> 
+        </>
+    )
+}
